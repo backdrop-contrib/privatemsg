fc90336adc513de6a1791f8194b593132a554b02
Alan Harder <mindless@33033.no-reply.drupal.org>
2007-02-13 19:28:34 +0000

    fix jquery syntax to get contacts box working again

--- privatemsg.js
+++ privatemsg.js
@@ -1,7 +1,7 @@
 function privatemsgContactFill() {
-  $('edit-quick').css('display', 'block');
-  $('edit-quick').change( function() {
-    $('edit-recipient').val(this.options[this.selectedIndex].value);
+  $('#edit-quick').css('display', 'block');
+  $('#edit-quick').change( function() {
+    $('#edit-recipient').val(this.options[this.selectedIndex].value);
     this.selectedIndex = 0;
   });
 }
