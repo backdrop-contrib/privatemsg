c1ab6ef9a9079e270e9a67eec2f87fd0ea5d8cfa
Karoly Negyesi <chx1975@gmail.com>
2007-07-20 11:25:11 +0000

    #125247 #141174 #140881 now on the correct branch

--- privatemsg.js
+++ privatemsg.js
@@ -1,11 +1,31 @@
-function privatemsgContactFill() {
-  $('#edit-quick').css('display', 'block');
-  $('#edit-quick').change( function() {
-    $('#edit-recipient').val(this.options[this.selectedIndex].value);
-    this.selectedIndex = 0;
-  });
-}
+// $Id$
 
 if (Drupal.jsEnabled) {
-  $(document).ready(privatemsgContactFill);
+  $(document).ready(
+    function() {
+      // Hide the elements we want hidden with JS.
+      $('.js-hide').hide(0);
+
+      // Show the elements we want to expose with JS.
+      $('.js-show').show(0);
+
+      // Add the JS to the delete button if necessary.
+      $('.pm-add-delete').click(
+        function() {
+          var response = confirm(pm_delete_msg);
+          if (response == true) {
+            $('#edit-js-bypass').val(1);
+          }
+          return response;
+        }
+      );
+
+      // Add the JS to the folder select if necessary.
+      $('.pm-add-folder-select').change(
+        function() {
+          this.form.submit();
+        }
+      );
+    }
+  );
 }
