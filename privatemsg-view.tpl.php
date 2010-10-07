fd521cbed193c8a96b8b6f7b00ee7b4c01bf0f74
Sascha Grossenbacher <saschagros@gmail.com>
2010-10-07 00:25:14 +0000

    #837748 by Berdir, BenK: Changed view page: cleaner HTML/CSS, display you for current user, improve overall design of displayed messages

--- privatemsg-view.tpl.php
+++ privatemsg-view.tpl.php
@@ -2,35 +2,27 @@
 // $Id$
 // Each file loads it's own styles because we cant predict which file will be
 // loaded.
-drupal_add_css(drupal_get_path('module', 'privatemsg') . '/styles/privatemsg-view.css');
+drupal_add_css(drupal_get_path('module', 'privatemsg') . '/styles/privatemsg-view.base.css');
+drupal_add_css(drupal_get_path('module', 'privatemsg') . '/styles/privatemsg-view.theme.css');
 ?>
 <?php print $anchors; ?>
-<div class="privatemsg-message" id="privatemsg-mid-<?php print $mid; ?>">
-  <div class="privatemsg-author-column">
-    <div class="privatemsg-author-column-left">
-      <div class="privatemsg-author-avatar">
-        <?php print $author_picture; ?>
-      </div>
-    </div>
-  </div>
-  <div class="privatemsg-author-column-right">
-    <div class="privatemsg-author-name">
-      <?php print $author_name_link; ?>
-    </div>
-    <div class="privatemsg-message-date">
-      <?php print $message_timestamp; ?>
-    </div>
+<div class="privatemsg-message<?php echo isset($new) ? ' privatemsg-message-new' : '' ?>" id="privatemsg-mid-<?php print $mid; ?>">
+  <div class="privatemsg-author-avatar">
+    <?php print $author_picture; ?>
   </div>
   <div class="privatemsg-message-column">
+    <?php if (isset($new)): ?>
+      <span class="new privatemsg-message-new"><?php print $new ?></span>
+    <?php endif ?>
+      <div class="privatemsg-message-information">
+        <span class="privatemsg-author-name"><?php print $author_name_link; ?></span> <span class="privatemsg-message-date"><?php print $message_timestamp; ?></span>
+        <?php if (isset($message_actions)): ?>
+          <?php print $message_actions ?>
+        <?php endif ?>
+      </div>
     <div class="privatemsg-message-body">
-      <?php if (isset($new)) : ?>
-        <span class="new"><?php print $new ?></span>
-      <?php endif ?>
       <?php print $message_body; ?>
     </div>
-    <?php if ( isset($message_actions)) : ?>
-       <?php print $message_actions ?>
-    <?php endif ?>
   </div>
-  <div class="clear-both privatemsg-bottom-border"></div>
+  <div class="clearfix"></div>
 </div>
