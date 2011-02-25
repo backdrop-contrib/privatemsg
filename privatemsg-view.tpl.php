bc34a729409d5ceadfaf7eb35d6c2aa38065f07a
The Great Git Migration <tggm@no-reply.drupal.org>
2011-02-25 02:06:01 +0000

    Stripping CVS keywords

--- privatemsg-view.tpl.php
+++ privatemsg-view.tpl.php
@@ -1,5 +1,4 @@
 <?php
-// $Id$
 // Each file loads it's own styles because we cant predict which file will be
 // loaded.
 drupal_add_css(drupal_get_path('module', 'privatemsg') . '/styles/privatemsg-view.base.css');
