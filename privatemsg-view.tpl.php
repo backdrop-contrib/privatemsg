331368ef957172207e2f8b4a1be2110775900ee1
Herb <herb@3speedhub.com>
2023-11-27 11:07:00 -0500

    Issue #46: use flexbox classes and update template and css (#47)

--- privatemsg-view.tpl.php
+++ privatemsg-view.tpl.php
@@ -1,22 +1,24 @@
 <?php
 print $anchors; ?>
-<div <?php if ( !empty($message_classes)) { ?>class="<?php echo implode(' ', $message_classes); ?>" <?php } ?> id="privatemsg-mid-<?php print $mid; ?>">
-  <div class="privatemsg-author-avatar">
-    <?php print $author_picture; ?>
-  </div>
-  <div class="privatemsg-message-column">
-    <?php if (isset($new)): ?>
-      <span class="new privatemsg-message-new"><?php print $new ?></span>
-    <?php endif ?>
+<div <?php if ( !empty($message_classes)) { ?>class="<?php echo implode(' ', $message_classes); ?> container" <?php } ?> id="privatemsg-mid-<?php print $mid; ?>">
+  <div class="row">
+    <div class="privatemsg-author-avatar col-md-2">
+      <?php print $author_picture; ?>
+      <span class="privatemsg-author-name"><?php print $author_name_link; ?></span><br/>
+      <span class="privatemsg-message-date"><?php print $message_timestamp; ?></span>
+      <?php if (isset($new)): ?>
+        <span class="new privatemsg-message-new"><?php print $new ?></span>
+      <?php endif ?>
+    </div>
+    <div class="privatemsg-message-column col-md-10">
       <div class="privatemsg-message-information">
-        <span class="privatemsg-author-name"><?php print $author_name_link; ?></span> <span class="privatemsg-message-date"><?php print $message_timestamp; ?></span>
         <?php if (isset($message_actions)): ?>
           <?php print $message_actions ?>
         <?php endif ?>
       </div>
-    <div class="privatemsg-message-body">
-      <?php print $message_body; ?>
+      <div class="privatemsg-message-body">
+        <?php print $message_body; ?>
+      </div>
     </div>
   </div>
-  <div class="clearfix"></div>
 </div>
